6ea2087c6d3f355f5de8f7e145e49605dd887d4a
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-07-27 11:41:52 +0800

    (feat) add authentification

--- src/components/authentification/AuthentificationForm.jsx
+++ src/components/authentification/AuthentificationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { axiosContext } from '../../contexts/AxiosContext';
 import { AuthContext } from '../../contexts/AuthContext';
 
@@ -6,30 +6,37 @@ function AuthentificationForm(props) {
     const [username, setUsername] = useState(null);
     const [password, setPassword] = useState(null);
 
+    const [authenftificationException, setAuthenftificationException] = useState([]);
+
+    const [usernameValidationException, setUsernameValidationException] = useState('');
+
     const { login } = useContext(AuthContext);
 
-    const authentificate = async function () {
-        try {
-            await axiosContext.post('/authentification/login', [username, password],
+    async function Authentificate() {
+            await axiosContext.post('/authentification/login', {username, password},
             ).then(response => {
-                if (response.data.isSuccess) {
+                if(response.data.isSuccess)
                     login(response.data.token, response.data.username);
-                }
-                else {
-                    alert(response.data.errors);
-                }
+                else
+                    setAuthenftificationException(response.data.errors);
+            }).catch(error => {
+                let validationErrors = error.response.data.errors;
+                setUsernameValidationException(validationErrors["Username"]);
             });
-        }
-        catch (error) {
-            console.log(error);
-        }
     };
+
+    useEffect(() => {
+        setUsernameValidationException('');
+    }, [username] 
+    )
 
     return (
         <div>
             <h3>Реестр ИБИДО</h3>
-            <form>
+            <p>{authenftificationException}</p>
+            <form onSubmit={e => e.preventDefault()}>
                 <label>
+                    <p>{usernameValidationException}</p>
                     <p>Username</p>
                     <input type='text'
                         onChange={e => setUsername(e.target.value)} />
@@ -40,7 +47,7 @@ function AuthentificationForm(props) {
                         onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
-                    <button type='submit' onClick={authentificate} onSubmit={e => e.preventDefault()}>Зайти</button>
+                    <button type='submit' onClick={Authentificate}>Зайти</button>
                 </div>
             </form>
         </div>
