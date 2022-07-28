7e5b9acf0e852e54a790de0cfe10330f0dc0661b
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-07-28 16:02:14 +0800

    add loader

---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Transition } from "react-transition-group";
+
+import styles from './loader.module.css';
+
+export default function Loader(){
+    return(
+        <div>
+            <circle>
+            </circle>
+        </div>
+    )
+}
