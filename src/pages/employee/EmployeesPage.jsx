606b525d450c2dd62380f0b1df016d8163136407
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-08-22 16:15:14 +0800

    (feat) add token to header

--- src/pages/employee/EmployeesPage.jsx
+++ src/pages/employee/EmployeesPage.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { AxiosContext } from "../../contexts/AxiosContext";
+
+import RegistryToolBox from "../../components/RegistyToolBox";
+import DataItem from '../../components/DataItem';
 
 import styles from '../../styles/Employees.module.css';
 
+
 export default function Employees(){
+    const [dataset, setDataset] = useState([]);
+
+    useEffect(() => {
+        AxiosContext('/employees')
+        .then(response => {
+            console.log(response.data);
+        });
+    });
+
     return(
         <main className={styles.Employees}>
-            <div>
-                <h1>Сотрудники</h1>
-            </div>
-            <div>
-            </div>
+            <h1 className={styles.title}>Сотрудники</h1>
+            <RegistryToolBox></RegistryToolBox>
+            <DataItem></DataItem>
         </main>
     );
 }
