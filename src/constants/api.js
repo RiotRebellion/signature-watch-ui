9a7090cb89a657fd8608d346df72d858671dee53
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-10-24 12:17:19 +0800

    (feat) add api consts

--- src/constants/api.js
+++ src/constants/api.js
@@ -5,3 +5,19 @@ export const SIGNATURES = '/signatures'
 export const LOGIN = '/authentification/login';
 
 export const REGISTRATION = '/authentification/registration';
+
+export const ACCORDANCE_SERTIFICATES = '/accordanseSertificates';
+
+export const CONTRACTS = '/contracts';
+
+export const DISTRIBUTIONS = '/distributions';
+
+export const FORMULARS = '/formulars';
+
+export const SOFTWARES = '/softwares';
+
+export const SOFTWARE_LICENSES = '/softwarelicenses';
+
+export const SOFTWARE_TYPES = '/softwaretypes';
+
+export const SUPPORTS = '/supports';
